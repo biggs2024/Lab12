22db74c473df85958fea6c783b194c0e4cd6ab8f
biggs2024 <ksandlin2021@gmail.com>
2026-05-01 14:40:22 -0400

    Create recursive method

--- src/Lab/Lab12Prob01.java
+++ src/Lab/Lab12Prob01.java
@@ -8,6 +8,19 @@ package Lab;
 
 public class Lab12Prob01 {
 	public static void main(String[] args) {
-		
+		System.out.print(recursiveAbstract(5));
+	}
+	
+	// Recursive method
+	public static int recursiveAbstract(int n) {
+		if (n == 0) {
+			return 1;
+		} else if (n == 1) {
+			return 3;
+		} else if (n == 2) {
+			return 4;
+		} else {
+			return recursiveAbstract(n-3) * (recursiveAbstract(n - 2) - recursiveAbstract(n - 1));
+		}
 	}
 }
